06864aea3aa3985933b81f105ea5c74458d1e05a
raphacnas <raphacnas@gmail.com>
2025-09-02 16:20:08 -0300

    Limelight's 'teleop' code creation

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,6 +6,10 @@ import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
 
 public class Robot extends TimedRobot {
 
@@ -16,9 +20,11 @@ public class Robot extends TimedRobot {
   private VictorSPX LMotor1 = new VictorSPX(1);
   private VictorSPX LMotor2 = new VictorSPX(2);
 
+  private NetworkTable limelight = NetworkTableInstance.getDefault().getTable("limelight");
+
   int POG;
-  boolean a, b, x, analog1, analog2, ltrigbool, rtrigbool, toggleA, toggleB, toggleX, lastA, lastB, lastX;
-  double spdbutton = 1, Lm, Rm, Ltrig, Rtrig, mag, mag2, sen, sen2, x1, x2, y1, y2;
+  boolean a, b, x, analog1, analog2, ltrigbool, rtrigbool, lastA=false, lastB=false, lastX=false;
+  double spdbutton = 1, Lm, Rm, Ltrig, Rtrig, mag, mag2, sen, sen2, x1, x2, y1, y2, tx, ta, tv;
 
   @Override
   public void robotInit() {
@@ -27,12 +33,18 @@ public class Robot extends TimedRobot {
 
     RMotor1.setInverted(true);
     LMotor1.setInverted(true);
+    RMotor2.setInverted(false);
+    LMotor2.setInverted(false);
 
     RMotor1.configNeutralDeadband(0.04);
     LMotor1.configNeutralDeadband(0.04);
+    RMotor2.configNeutralDeadband(0.04);
+    LMotor2.configNeutralDeadband(0.04);
 
     RMotor1.setNeutralMode(NeutralMode.Brake);
     LMotor1.setNeutralMode(NeutralMode.Brake);
+    RMotor2.setNeutralMode(NeutralMode.Brake);
+    LMotor2.setNeutralMode(NeutralMode.Brake);
   }
 
   @Override
@@ -40,9 +52,9 @@ public class Robot extends TimedRobot {
 
     a = joydelicio.getRawButton(1);
     b = joydelicio.getRawButton(2);
-    x = joydelicio.getRawButton(3);
+    x = joydelicio.getRawButton(4);
     
-    Ltrig = -joydelicio.getRawAxis(3);
+    Ltrig = joydelicio.getRawAxis(3);
     Rtrig = joydelicio.getRawAxis(2);
 
     x1 = joydelicio.getRawAxis(0); 
@@ -60,86 +72,135 @@ public class Robot extends TimedRobot {
     sen2 = y2 / mag2;
 
     POG = joydelicio.getPOV();
-
-    // BOTÃO + LÓGICA DE TOGGLE
-    if (a && !lastA) toggleA = !toggleA; 
-    if (b && !lastB) toggleB = !toggleB; 
-    if (x && !lastX) toggleX = !toggleX; 
-
-    lastA = a;
-    lastB = b;
-    lastX = x;
-
-    spdbutton = toggleA ? 0.25 : toggleB ? 0.5 : toggleX ? 1.0 : 1.0;
     
+    CalcButton();    
     
     if (POG != -1){
       CalcPov();
-    }
+    } else {
 
-    
-    if (Ltrig > 0.04 && !rtrigbool){
-      CalcLTrig();
-    } else if (Rtrig > 0.04 && !ltrigbool){
-      CalcRTrig();
-    }
+      if (Ltrig > 0.04 && !rtrigbool){
+        CalcLTrig();
+      } else if (Rtrig > 0.04 && !ltrigbool){
+        CalcRTrig();
+      } else {
 
+        if (mag != 0 && !analog2) {
+          analog1 = true;
+          analog2 = false;
+          CalcAnalog1();
     
-    if (mag != 0 && !analog2) {
-      analog1 = true;
-      analog2 = false;
-      CalcAnalog1();
-    } else if (mag2 != 0 && !analog1) {
-      analog1 = false;
-      analog2 = true;
-      CalcAnalog2();
-    } if (analog1 == false && analog2 == false) {
-      Rm = 0;
-      Lm = 0;
+        } else if (mag2 != 0 && !analog1) {
+          analog1 = false;
+          analog2 = true;
+          CalcAnalog2();
+    
+        } 
+      }
     }
 
     RMotor1.set(ControlMode.PercentOutput, Rm);
     LMotor1.set(ControlMode.PercentOutput, Lm);
 
+    Smartdashboard();
+
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+
+    CalcFollower();
+    
+    RMotor1.set(ControlMode.PercentOutput, Rm);
+    LMotor1.set(ControlMode.PercentOutput, Lm);
+
+    Smartdashboard();
+}
+
+  public void CalcFollower() {
+    tx = limelight.getEntry("tx").getDouble(0.0);
+    ta = limelight.getEntry("ta").getDouble(0.0);
+    tv = limelight.getEntry("tv").getDouble(0.0); 
+  
+    if (tv < 1.0) {
+      Lm = 0;
+      Rm = 0;
+      return;
+    }
+  
+    // Ajustes finos
+    double rot_percent = 0.02;     
+    double fwd_percent = 0.1;      
+    double targetArea = 5.0;  
+  
+    double rot = rot_percent * tx; 
+    double forward = fwd_percent * (targetArea - ta);
+  
+    forward = Math.max(-0.6, Math.min(forward, 0.6));
+    rot = Math.max(-0.4, Math.min(rot, 0.4));
+  
+    Lm = forward + rot;
+    Rm = forward - rot;
+  }
+
+  public void CalcButton(){
+    // Toggle para A
+    if (a && !lastA) {
+      spdbutton = 0.25;
     }
 
-    public void CalcLTrig(){
-      if (Rtrig < 0.04 && Ltrig >= 0.04) {
-        Ltrig *= spdbutton;
-        ltrigbool = true;
-      } 
+    // Toggle para B
+    if (b && !lastB) {
+        spdbutton = 0.5;
     }
 
-
-    public void CalcRTrig(){
-      if (Ltrig < 0.04 && Rtrig >= 0.04) {
-        Rtrig *= spdbutton;
-        rtrigbool = true;
-      }
+    // Toggle para X
+    if (x && !lastX) {
+        spdbutton = 1.0;
     }
 
+    lastA = a;
+    lastB = b;
+    lastX = x;
+  }
 
-    public void CalcAnalog1(){
-      // QUADRANTE 1
-      if (x1 >= 0 && y1 >= 0) {
-        Lm = mag * spdbutton;
-        Rm = (2 * sen - 1) * mag * spdbutton;
-      }
-      // QUADRANTE 2
-      else if (x1 < 0 && y1 >= 0) {
-          Lm = (2 * sen - 1) * mag * spdbutton;
-          Rm = mag * spdbutton;
-      }
-      // QUADRANTE 3
-      else if (x1 >= 0 && y1 < 0) {
-          Lm = -mag * spdbutton;
-          Rm = (2 * sen + 1) * mag * spdbutton;
-      }
-      // QUADRANTE 4
-      else if (x1 < 0 && y1 < 0) {
-          Lm = (2 * sen + 1) * mag * spdbutton;
-          Rm = -mag * spdbutton;
-      }
+  public void CalcLTrig(){
+    if (Rtrig < 0.04 && Ltrig >= 0.04) {
+      Ltrig *= -spdbutton;
+      Lm = Ltrig; Rm = Ltrig;
+      ltrigbool = true;
+    } 
+  }
+
+  public void CalcRTrig(){
+    if (Ltrig < 0.04 && Rtrig >= 0.04) {
+      Rtrig *= spdbutton;
+      Lm = Rtrig; Rm = Rtrig;
+      rtrigbool = true;
+    }
+  }
+
+  public void CalcAnalog1(){
+    // QUADRANTE 1
+    if (x1 >= 0 && y1 >= 0) {
+      Lm = mag * spdbutton;
+      Rm = (2 * sen - 1) * mag * spdbutton;
+    }
+    // QUADRANTE 2
+    else if (x1 < 0 && y1 >= 0) {
+        Lm = (2 * sen - 1) * mag * spdbutton;
+        Rm = mag * spdbutton;
+    }
+    // QUADRANTE 3
+    else if (x1 >= 0 && y1 < 0) {
+        Lm = -mag * spdbutton;
+        Rm = (2 * sen + 1) * mag * spdbutton;
+    }
+    // QUADRANTE 4
+    else if (x1 < 0 && y1 < 0) {
+        Lm = (2 * sen + 1) * mag * spdbutton;
+        Rm = -mag * spdbutton;
+    }
   }
 
   public void CalcAnalog2(){
@@ -200,11 +261,17 @@ public class Robot extends TimedRobot {
         Rm = 0.40;
         Lm = 0;
         break;
-
-      default:
-      case -1:
-       Rm = 0;
-       Lm = 0;
     }
+  }
+
+  public void Smartdashboard(){
+    SmartDashboard.putNumber("1 - Tx", tx);
+    SmartDashboard.putNumber("2 - Ta", ta);
+    SmartDashboard.putNumber("3 - Tv", tv);
+    SmartDashboard.putNumber("* - Lm", Lm);
+    SmartDashboard.putNumber("* - Rm", Rm);
+    SmartDashboard.putNumber("** - SpdButton", spdbutton);
+    SmartDashboard.putNumber("4 - Ltrig", Ltrig);
+    SmartDashboard.putNumber("5 - Rtrig", Rtrig);
   }
 }
